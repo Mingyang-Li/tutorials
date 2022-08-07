491858b6bde4f5b0e026f8581f15e52ed7f30010
Mingyang Li <mingyangli118@gmail.com>
2022-08-07 22:53:41 +1200

    doc: init questions prompts

---
+++ tutorial.py
@@ -0,0 +1,15 @@
+# write a function that takes in the length of a square and returns the area of that square
+def calculate_square_area(length):
+    return length * length
+
+# write a function that takes in a radius and return the area of a circle
+def calculate_circle(radius):
+    pass
+
+# write a function that takes in an array of numbers and return an array where all elements are twice as big as the item on the same index of the original array
+def twice_as_big(array):
+    pass
+
+# write a function that takes in an array, and returns an array of every SECOND element of the original array
+def every_second_element(array):
+    pass
